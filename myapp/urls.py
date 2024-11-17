51daea01147bceccad59ed59401aba7ec4252259
AngeloParungao <angeloparungao.ap@gmail.com>
2024-11-17 23:39:46 +0800

    feat: add all the routes

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("articles", views.articles, name="articles"),
+    path("activities", views.activities, name="activities"),
+    path("multimedia", views.multimedia, name="multimedia"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact")
 ]
