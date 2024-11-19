277662cd01afdc2b38f900ec0c3bd187d1c9a03c
AngeloParungao <angeloparungao.ap@gmail.com>
2024-11-19 16:01:57 +0800

    feat: slug

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,18 @@
 # models.py
 from django.db import models
+from django.utils.text import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='articles/images/', blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)  # Add slug field
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)  # Auto-generate slug from the title
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
