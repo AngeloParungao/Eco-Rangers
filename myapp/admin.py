69290bb4a39c645d4032e61dc3578c987c052bf4
AngeloParungao <angeloparungao.ap@gmail.com>
2024-11-19 13:09:56 +0800

    feat: add subtopics and activities for every article

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,37 @@
 # admin.py
 from django.contrib import admin
-from .models import Article
+from .models import Article, Subtopic, Activity
+
+
+class ActivityInline(admin.TabularInline):  # Use TabularInline or StackedInline
+    model = Activity
+    extra = 1  # Number of empty activity forms shown by default
+
+
+class SubtopicInline(admin.StackedInline):  # For a detailed view
+    model = Subtopic
+    extra = 1
+    inlines = [ActivityInline]  # Nest ActivityInline inside SubtopicInline
+
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'date')
     search_fields = ('title', 'content')
     list_filter = ('date',)
+    inlines = [SubtopicInline]  # Add Subtopic inline editing
+
+
+@admin.register(Subtopic)
+class SubtopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'article')
+    search_fields = ('title', 'description')
+    list_filter = ('article',)
+    inlines = [ActivityInline]  # Add Activity inline editing
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtopic', 'activity_type')
+    search_fields = ('title', 'description')
+    list_filter = ('activity_type',)
--- myapp/models.py
+++ myapp/models.py
@@ -9,3 +9,29 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Subtopic(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='subtopics')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='subtopics/images/', blank=True, null=True)  # Add image field
+
+    def __str__(self):
+        return self.title
+
+
+class Activity(models.Model):
+    ACTIVITY_TYPES = [
+        ('game', 'Game'),
+        ('quiz', 'Quiz'),
+        ('discussion', 'Discussion'),
+    ]
+
+    subtopic = models.ForeignKey(Subtopic, on_delete=models.CASCADE, related_name='activities')
+    title = models.CharField(max_length=255)
+    activity_type = models.CharField(max_length=50, choices=ACTIVITY_TYPES)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.get_activity_type_display()})"
